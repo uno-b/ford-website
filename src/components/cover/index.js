500ce3c74fd3e9348d4cc05a5d6f1b846fdb6c23
Unumandakh <bg.unuu@yahoo.com>
2021-01-23 23:58:49 +0800

    Reduced whitespace between sections and added object-fit property to images

--- src/components/cover/index.js
+++ src/components/cover/index.js
@@ -1,4 +1,6 @@
 import React from "react"
+import scrollTo from "gatsby-plugin-smoothscroll"
+
 import Image from "../image"
 import styles from "./cover.module.css"
 
@@ -10,7 +12,7 @@ const Cover = () => {
         <div className={styles.container}>
           <h1>Get Ford M & V Serial Radio Unlock Code Instantly</h1>
           <p>!!! 1 USD Per Code !!!</p>
-          <button>Get Ford Code!</button>
+          <button onClick={() => scrollTo("#code")}>Get Ford Code!</button>
         </div>
       </div>
     </>

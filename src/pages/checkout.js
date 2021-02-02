7aa644175657c37d076b5c23a02a822bff6f72c0
Unumandakh <bg.unuu@yahoo.com>
2021-02-02 13:58:45 +0800

    Added paypal button

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,10 +2,12 @@ import React from "react"
 
 import Footer from "../components/footer"
 import SEO from "../components/seo"
+import Paypal from "../components/paypal"
 
 const IndexPage = () => (
   <>
     <SEO title="Contact Us" />
+    <Paypal />
     <Footer />
   </>
 )
